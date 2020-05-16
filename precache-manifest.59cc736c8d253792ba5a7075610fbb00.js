3cb0bd740e62afaff5415db2271308ff8f87efa9
Duarte Monteiro <du5rte@users.noreply.github.com>
2020-05-17 00:38:46 +0100

    Updates

---
+++ precache-manifest.59cc736c8d253792ba5a7075610fbb00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf1d38d1fe26bf026b77120d53531d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bbe587b077b4bc3e9dc",
+    "url": "/static/css/main.9be4a0b7.chunk.css"
+  },
+  {
+    "revision": "00978c0b18794da64bfd",
+    "url": "/static/js/2.555be4ca.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/static/js/2.555be4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbe587b077b4bc3e9dc",
+    "url": "/static/js/main.e50b8543.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
